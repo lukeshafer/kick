f509b712e429e28f18f9ca7fbf49e31a223f569b
Luke Shafer <lukealanshafer@gmail.com>
2022-11-12 01:06:01 -0500

    Initial commit

---
+++ src/components/LitleGuy.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let imgPos = 0
+	const move = (dir: "left" | "right") => {
+		const moveDist = dir === "left" ? -10 : 10;
+		imgPos = imgPos + moveDist;
+	}
+
+	const handleKeydown = (e: KeyboardEvent) => {
+		switch (e.key) {
+			case "ArrowLeft": 
+				move("left");
+				break;
+			case "ArrowRight":
+				move("right");
+		}
+	}
+
+</script>
+
+<svelte:window on:keydown={handleKeydown} />
+
+<img style:transform={`translateX(${imgPos}px)`} src="/pixil-f0.png" alt="" width={128} height={128} />
+
